041f7c45aa3c670b003b3c3edf621ea59ad525f0
Krzysztof Bogusz <krzysztofbogusz050591@gmail.com>
2018-03-26 12:06:43 +0200

    Add files via upload

---
+++ CodeWars - Kata - 6 kyu/CodeWars - Bit counting- 6kyu .js
@@ -0,0 +1,7 @@
+// Write a function that takes an (unsigned) integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+var countBits = function(n) {
+  return n.toString(2).split("").filter(a => a !=="0").length;
+};
